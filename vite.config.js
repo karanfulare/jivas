32416cad04003547595861b3e5ee58a188edcd31
KaranFulare <fularekaran@yahoo.com>
2025-05-24 12:15:35 +0530

    menu changed

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['e445-2401-4900-1f26-79d2-3cfe-8566-1baf-4e93.ngrok-free.app'] // allows all hosts (useful for ngrok, localtunnel, etc.)
+    allowedHosts: ['5e87-49-207-195-126.ngrok-free.app'] // allows all hosts (useful for ngrok, localtunnel, etc.)
   }
 })
